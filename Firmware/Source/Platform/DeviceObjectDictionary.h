3d4904f52b230694104da788e632d7919595ad4f
d.ivanov <d.ivanov@proton-electrotex.com>
2023-01-19 13:18:11 +0300

    Удалены лишние регистры

--- Firmware/Source/Platform/DeviceObjectDictionary.h
+++ Firmware/Source/Platform/DeviceObjectDictionary.h
@@ -26,73 +26,57 @@
 
 // Регистры
 // Сохраняемые регистры
-#define REG_CURRENT_THRESHOLD_RANGE0	0	// Верхний порог по току диапазона 0 (А * 10)
-#define REG_CURRENT_THRESHOLD_RANGE1	1	// Верхний порог по току диапазона 1 (А * 10)
-#define REG_BATTERY_VOLTAGE_THRESHOLD	2	// Порог заряда батареи конденсаторов (В * 10)
-#define REG_DAC_OUTPUT_LIMIT_VALUE		3	// Ограничение выхода ЦАП (0 - 4095)
-#define REG_BATTERY_FULL_CHRAGE_TIMEOUT	4	// Время таймаута полного заряда конденсаторов (мс)
-#define REG_BATTERY_RECHARGE_TIMEOUT	5	// Время таймаута дозаряда после импульса (мс)
-#define REG_SHUNT_RESISTANCE			6	// Сопротивление шунта (мкОм)
-#define REG_REGULATOR_QI_MAX			7	// Ограничение уровня интегральной составляющей
-#define REG_FAN_CTRL					8	// Включение управления вентилятором
-#define REG_AFTER_PULSE_PAUSE			9	// Пауза после импульса (мс)
-#define REG_CURRENT_PER_CURBOARD		10	// Максимальная амплитуда тока с одной CurrentBoard (А * 10)
-#define REG_CURBOARD_QUANTITY			11	// Количество CurrentBoard
-#define REG_SCOPE_STEP					12	// Шаг сохранения оцифрованных значений
-#define REG_FAN_OPERATE_PERIOD			13	// Период работы вентилятора (сек)
-#define REG_FAN_OPERATE_TIME			14	// Время включенного состояния вентилятора (сек)
-#define REG_DAC_OFFSET					15	// Смещение сигнала с ЦАП
-#define REG_CONFIG_READY_STATE_TIMEOUT	16	// Таймаут нахождения блока в состоянии DS_ConfigReady
-
+#define REG_CURRENT_THRESHOLD			0	// Значение тока переключения диапазонов (А)
+#define REG_BATTERY_VOLTAGE_THRESHOLD	1	// Порог заряда батареи конденсаторов (В)
+#define REG_DAC_OUTPUT_LIMIT_VALUE		2	// Ограничение выхода ЦАП (0 - 4095)
+#define REG_BATTERY_FULL_CHRAGE_TIMEOUT	3	// Время таймаута полного заряда конденсаторов (мс)
+#define REG_BATTERY_RECHARGE_TIMEOUT	4	// Время таймаута дозаряда после импульса (мс)
+#define REG_SHUNT_RESISTANCE			5	// Сопротивление шунта (мкОм)
+#define REG_REGULATOR_QI_MAX			6	// Ограничение уровня интегральной составляющей
+#define REG_FAN_CTRL					7	// Включение управления вентилятором
+#define REG_AFTER_PULSE_PAUSE			8	// Пауза после импульса (мс)
+#define REG_CURRENT_PER_CURBOARD		9	// Максимальная амплитуда тока с одной CurrentBoard (А)
+#define REG_CURBOARD_QUANTITY			10	// Количество CurrentBoard
+#define REG_SCOPE_STEP					11	// Шаг сохранения оцифрованных значений
+#define REG_FAN_OPERATE_PERIOD			12	// Период работы вентилятора (сек)
+#define REG_FAN_OPERATE_TIME			13	// Время включенного состояния вентилятора (сек)
+#define REG_DAC_OFFSET					14	// Смещение сигнала с ЦАП
+#define REG_CONFIG_READY_STATE_TIMEOUT	15	// Таймаут нахождения блока в состоянии DS_ConfigReady
+//
 #define REG_I_TO_DAC_RANGE0_K			20	// Диапазон 0 - Коэффициент пересчета в значение ЦАП
 #define REG_I_TO_DAC_RANGE0_B			21	// Диапазон 0 - Смещение пересчета в значение ЦАП
 #define REG_I_TO_DAC_RANGE1_K			22	// Диапазон 1 - Коэффициент пересчета в значение ЦАП
 #define REG_I_TO_DAC_RANGE1_B			23	// Диапазон 1 - Смещение пересчета в значение ЦАП
-#define REG_I_TO_DAC_RANGE2_K			24	// Диапазон 2 - Коэффициент пересчета в значение ЦАП
-#define REG_I_TO_DAC_RANGE2_B			25	// Диапазон 2 - Смещение пересчета в значение ЦАП
 //
-#define REG_ADC_VOLTAGE_K				26	// Измерение напряжения на батарее, пропорциональный коэффициент K
-#define REG_ADC_VOLTAGE_B				27	// Измерение напряжения на батарее, коэффициент cмещения B (в Вольтах)
+#define REG_ADC_VOLTAGE_K				24	// Измерение напряжения на батарее, пропорциональный коэффициент K
+#define REG_ADC_VOLTAGE_B				25	// Измерение напряжения на батарее, коэффициент cмещения B
 //
-#define REG_K_AMP_RANGE0				28	// Коэффициент усиления тока в канале 0 (*100)
-#define REG_K_AMP_RANGE1				29	// Коэффициент усиления тока в канале 1 (*100)
-#define REG_K_AMP_RANGE2				30	// Коэффициент усиления тока в канале 2 (*100)
+#define REG_K_AMP_RANGE0				26	// Коэффициент усиления тока в канале 0
+#define REG_K_AMP_RANGE1				27	// Коэффициент усиления тока в канале 1
 //
-#define REG_ADC_I_RANGE0_P2				31	// Диапазон 0 - Коэффициент тонкой подстройки Р2 х1е6
-#define REG_ADC_I_RANGE0_P1				32	// Диапазон 0 - Коэффициент тонкой подстройки Р1 x1000
-#define REG_ADC_I_RANGE0_P0				33	// Диапазон 0 - Смещение тонкой подстройки Р0
-#define REG_ADC_I_RANGE0_N				34	// Диапазон 0 - Числитель коэффициента грубой подстройки
-#define REG_ADC_I_RANGE0_D				35	// Диапазон 0 - Знаменатель коэффициента грубой подстройки
-#define REG_ADC_I_RANGE0_B				36	// Диапазон 0 - Смещение тонкой подстройки B
+#define REG_ADC_I_RANGE0_P2				30	// Диапазон 0 - Коэффициент тонкой подстройки Р2
+#define REG_ADC_I_RANGE0_P1				31	// Диапазон 0 - Коэффициент тонкой подстройки Р1
+#define REG_ADC_I_RANGE0_P0				32	// Диапазон 0 - Смещение тонкой подстройки Р0
+#define REG_ADC_I_RANGE0_K				33	// Диапазон 0 - Коэффициент грубой подстройки K
+#define REG_ADC_I_RANGE0_B				34	// Диапазон 0 - Смещение грубой подстройки B
 //
-#define REG_ADC_I_RANGE1_P2				37	// Диапазон 1 - Коэффициент тонкой подстройки Р2 х1е6
-#define REG_ADC_I_RANGE1_P1				38	// Диапазон 1 - Коэффициент тонкой подстройки Р1 x1000
-#define REG_ADC_I_RANGE1_P0				39	// Диапазон 1 - Смещение тонкой подстройки Р0
-#define REG_ADC_I_RANGE1_N				40	// Диапазон 1 - Числитель коэффициента грубой подстройки
-#define REG_ADC_I_RANGE1_D				41	// Диапазон 1 - Знаменатель коэффициента грубой подстройки
-#define REG_ADC_I_RANGE1_B				42	// Диапазон 1 - Смещение тонкой подстройки B
+#define REG_ADC_I_RANGE1_P2				35	// Диапазон 1 - Коэффициент тонкой подстройки Р2
+#define REG_ADC_I_RANGE1_P1				36	// Диапазон 1 - Коэффициент тонкой подстройки Р1
+#define REG_ADC_I_RANGE1_P0				37	// Диапазон 1 - Смещение тонкой подстройки Р0
+#define REG_ADC_I_RANGE1_K				38	// Диапазон 1 - Коэффициент грубой подстройки K
+#define REG_ADC_I_RANGE1_B				39	// Диапазон 1 - Смещение тонкой подстройки B
 //
-#define REG_ADC_I_RANGE2_P2				43	// Диапазон 2 - Коэффициент тонкой подстройки Р2 х1е6
-#define REG_ADC_I_RANGE2_P1				44	// Диапазон 2 - Коэффициент тонкой подстройки Р1 x1000
-#define REG_ADC_I_RANGE2_P0				45	// Диапазон 2 - Смещение тонкой подстройки Р0
-#define REG_ADC_I_RANGE2_N				46	// Диапазон 2 - Числитель коэффициента грубой подстройки
-#define REG_ADC_I_RANGE2_D				47	// Диапазон 2 - Знаменатель коэффициента грубой подстройки
-#define REG_ADC_I_RANGE2_B				48	// Диапазон 2 - Смещение тонкой подстройки B
+#define REG_REGULATOR_RANGE0_Kp			40	// Диапазон 0 - Пропорциональный коэффициент регулятора
+#define REG_REGULATOR_RANGE0_Ki			41	// Диапазон 0 - Интегральный коэффициент регулятора
+#define REG_REGULATOR_RANGE1_Kp			42	// Диапазон 1 - Пропорциональный коэффициент регулятора
+#define REG_REGULATOR_RANGE1_Ki			43	// Диапазон 1 - Интегральный коэффициент регулятора
 //
-#define REG_REGULATOR_RANGE0_Kp			49	// Диапазон 0 - Пропорциональный коэффициент регулятора
-#define REG_REGULATOR_RANGE0_Ki			50	// Диапазон 0 - Интегральный коэффициент регулятора
-#define REG_REGULATOR_RANGE1_Kp			51	// Диапазон 1 - Пропорциональный коэффициент регулятора
-#define REG_REGULATOR_RANGE1_Ki			52	// Диапазон 1 - Интегральный коэффициент регулятора
-#define REG_REGULATOR_RANGE2_Kp			53	// Диапазон 2 - Пропорциональный коэффициент регулятора
-#define REG_REGULATOR_RANGE2_Ki			54	// Диапазон 2 - Интегральный коэффициент регулятора
-//
-#define REG_REGULATOR_TF_Ki_RANG0		55	// Диапазон 0 - Коэффициент подстройки значения Ki от тока (в ед. (dKi / dI) * 1000)
-#define REG_REGULATOR_TF_Ki_RANG1		56	// Диапазон 1 - Коэффициент подстройки значения Ki от тока (в ед. (dKi / dI) * 1000)
-#define REG_REGULATOR_TF_Ki_RANG2		57	// Диапазон 2 - Коэффициент подстройки значения Ki от тока (в ед. (dKi / dI) * 1000)
+#define REG_REGULATOR_TF_Ki_RANG0		55	// Диапазон 0 - Коэффициент подстройки значения Ki от тока (в ед. (dKi / dI))
+#define REG_REGULATOR_TF_Ki_RANG1		56	// Диапазон 1 - Коэффициент подстройки значения Ki от тока (в ед. (dKi / dI))
 
 // Несохраняемые регистры чтения-записи
-#define REG_CURRENT_PULSE_VALUE			128	// Задание амплитуды импульса тока (А * 10)
-#define REG_USE_LINEAR_DOWN				130	// Спад тока идёт по линейному закону
+#define REG_CURRENT_PULSE_VALUE			128	// Задание амплитуды импульса тока (А)
+#define REG_USE_LINEAR_DOWN				129	// Спад тока идёт по линейному закону
 
 #define REG_DBG							150	// Регистр режима Отладки
 
@@ -105,8 +89,8 @@
 #define REG_OP_RESULT					197	// Регистр результата операции
 #define REG_SUB_STATE					198	// Регистр вспомогательного состояния
 
-#define REG_RESULT_CURRENT				200	// Достигнутое значение импульса тока (А * 10)
-#define REG_BATTERY_VOLTAGE				201	// Напряжение на батарее конденсаторов (В * 10)
+#define REG_RESULT_CURRENT				200	// Достигнутое значение импульса тока (А)
+#define REG_BATTERY_VOLTAGE				201	// Напряжение на батарее конденсаторов (В)
 // -----------------------------
 #define REG_FWINFO_SLAVE_NID			256	// Device CAN slave node ID
 #define REG_FWINFO_MASTER_NID			257	// Device CAN master node ID (if presented)
