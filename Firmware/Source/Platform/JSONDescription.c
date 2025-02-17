79c28c3e8c79e29cf50821c78200655ebb881099
Alexander-Mamasaklisov <a.mamasakhlisov@proton-electrotex.com>
2025-02-17 15:02:47 +0300

    Лишние элементы библиотеки спрятаны из заголовочных файлов

--- Firmware/Source/Platform/JSONDescription.c
+++ Firmware/Source/Platform/JSONDescription.c
@@ -1,5 +1,4 @@
 #include "JSONDescription.h"
-#include "FormatOutputJSON.h"
 
 const char TemplateJSON[] = "[{\n"
 "	'param' : 'Itm',\n"
@@ -22,11 +21,3 @@ const char TemplateJSON[] = "[{\n"
 "}]";
 
 float Itm1Min, Itm1Max, Itm2Min, Itm2Max;
-
-void* JSONPointers[JSON_POINTERS_SIZE] = {0};
-
-void JSON_AssignPointer(Int16U Index, pFloat32 Pointer)
-{
-	if (Index < JSON_POINTERS_SIZE)
-		JSONPointers[Index] = Pointer;
-}
--- Firmware/Source/Platform/JSONDescription.h
+++ Firmware/Source/Platform/JSONDescription.h
@@ -1,14 +1,9 @@
 #ifndef __JSON_DESCRIPTION_H
 #define __JSON_DESCRIPTION_H
 
-#include "Global.h"
-
-#define JSON_POINTERS_SIZE 32
+#include "stdinc.h"
+#include "FormatOutputJSON.h"
 
 extern float Itm1Min, Itm1Max, Itm2Min, Itm2Max;
-
-extern void* JSONPointers[JSON_POINTERS_SIZE];
-
-void JSON_AssignPointer(Int16U Index, pFloat32 Pointer);
 
 #endif // __JSON_DESCRIPTION_H
