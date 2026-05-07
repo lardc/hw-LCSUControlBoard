f02c083d11858cc1e5a2d46901acf200de306e7a
A-Kuptsova <cuptsowa.n1997@gmail.com>
2026-05-07 16:48:40 +0300

    Инвертирована логика включения GPIO_OUT_LOCK

--- Firmware/Source/Controller/Controller.c
+++ Firmware/Source/Controller/Controller.c
@@ -120,7 +120,7 @@ void CONTROL_ResetToDefaultState()
 {
 	CONTROL_ResetOutputRegisters();
 	
-	LL_CurrentBoardLock(true);
+	LL_CurrentBoardLock(false);
 	LL_PowerSupplyEnable(false);
 
 	CONTROL_SetDeviceState(DS_None, SS_None);
@@ -414,7 +414,7 @@ void CONTROL_StopProcess()
 {
 	TIM_Stop(TIM15);
 	LL_WriteDAC(0);
-	LL_CurrentBoardLock(true);
+	LL_CurrentBoardLock(false);
 	LL_OutputAmplifierOffset(true);
 	INITCFG_ADC1SoftTrig(true);
 
@@ -448,7 +448,7 @@ void CONTROL_StartProcess()
 	CONTROL_HandleExternalLamp(true);
 
 	LL_OutputAmplifierOffset(false);
-	LL_CurrentBoardLock(false);
+	LL_CurrentBoardLock(true);
 	DELAY_US(DAC_UNLOCK_STAB_TIME);
 
 	INITCFG_ADC1SoftTrig(false);
